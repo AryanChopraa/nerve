581fa55d535fd1cdd027d379cb5db32d32346a3a
‘aryan <aryanchopra989@gmail.com>
2023-07-21 04:44:49 +0530

    added close button

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -39,7 +39,6 @@
     SelectedCardStore.set(index);
   }
 
-  // Filter Functionality
   let isFilterModalOpen = false;
   let experienceFilter = 0;
 
@@ -69,7 +68,9 @@
       height="50"
       viewBox="0 0 50 50"
     >
-      <!-- ... SVG path for collapsible button ... -->
+      <path
+        d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"
+      />
     </svg>
   </button>
 
